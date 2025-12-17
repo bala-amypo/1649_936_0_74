fc285299916abecf5207baaaf1f90c2a20ac3506
bala-amypo <bala@amypo.in>
2025-12-17 09:54:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -4,5 +4,8 @@ import org.springframework.web.bind.annotation.Autowired;
 @RestController
 public class StudentController{
     @Autowired StudentService ser;
-    
+    @PostMapping("/post")
+    public StudentEntity sendData(){
+        
+    }
 }
