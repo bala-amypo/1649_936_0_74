d5145157b27766647c526ecad8d41b12ce469cd5
bala-amypo <bala@amypo.in>
2025-12-18 04:36:26 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -13,21 +13,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9123.408procr.amypo.ai/")
+                        new Server().url("https://9015.408procr.amypo.ai/")
                 ));
         }
-}package com.example.demo.service.impl;
-import com.example.demo.service.StudentService;
-import com.example.demo.repository.StudentRepo;
-import com.example.demo.entity.StudentEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Override;
-@Service
-public class StudentServiceImpl implements StudentService{
-     @Autowired StudentRepo student;
-     @Override
-      public StudentEntity postData(StudentEntity stu){
-          return student.save(stu);
-     }
-}
+};
