56a5df09454854c8f5e64d7a6e7f5e2d6490c53e
bala-amypo <bala@amypo.in>
2025-12-18 06:11:27 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -6,5 +6,5 @@ public interface StudentService{
     List<StudentEntity> getData();
     String deleteData(int id);
     StudentEntity findData(int id);
-    StudentEntity updateData()
+    StudentEntity updateData(int id,StudentEntity s);
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -26,4 +26,8 @@ public class StudentServiceImpl implements StudentService{
           public StudentEntity findData(int id){
                return student.findById(id).orElse(null);
           }
+          @Override
+          public StudentEntity updateData(int id,StudentEntity s){
+               if( )
+          }
 }
