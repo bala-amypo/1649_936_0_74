a6e090c0fccf02bc9a6b4ad7cd84af8b20c3928b
bala-amypo <bala@amypo.in>
2025-12-19 05:37:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/GobalException.java
+++ src/main/java/com/example/demo/exception/GobalException.java
@@ -1,11 +1,12 @@
 package com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 @RestControllerAdvice
 public class GobalException{
     @ExceptionHandler(ValidationException.class)
     public ResponseBody<String> handleValidationException(ValidationException ex){
-        return new REsponse
+        return new ResponseBody<String>(ex.getMessage(),HttpStatus.BAD_GATEWAY);
     }
 
 }
