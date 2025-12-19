0050c8332816bccd6d762c550e2ff5d7a042b9f8
bala-amypo <bala@amypo.in>
2025-12-19 06:31:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.demo.entity.ValidationEntity;
 import com.example.demo.service.ValidationService;
 import jakarta.validation.Valid;
--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -12,43 +12,43 @@ public class StudentEntity
     private Integer id;
     private String name,email,password;
     private Date created;
-    public StudentEntity(){
-    }
-    public StudentEntity(Integer id,String name,String email,String password,Date created){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.created=created;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setId(Integer id){
-        this.id=id;
-    }
-    public void setCreated(Date created){
-        this.created=created;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public Integer getId(){
-        return id;
-    }
-    public Date getCreated(){
-        return created;
-    }
+    // public StudentEntity(){
+    // }
+    // public StudentEntity(Integer id,String name,String email,String password,Date created){
+    //     this.id=id;
+    //     this.name=name;
+    //     this.email=email;
+    //     this.password=password;
+    //     this.created=created;
+    // }
+    // public void setName(String name){
+    //     this.name=name;
+    // }
+    // public void setEmail(String email){
+    //     this.email=email;
+    // }
+    // public void setPassword(String password){
+    //     this.password=password;
+    // }
+    // public void setId(Integer id){
+    //     this.id=id;
+    // }
+    // public void setCreated(Date created){
+    //     this.created=created;
+    // }
+    // public String getName(){
+    //     return name;
+    // }
+    // public String getEmail(){
+    //     return email;
+    // }
+    // public String getPassword(){
+    //     return password;
+    // }
+    // public Integer getId(){
+    //     return id;
+    // }
+    // public Date getCreated(){
+    //     return created;
+    // }
 }
