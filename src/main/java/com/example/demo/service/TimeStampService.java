08137a1d93c8f8f1922ac1f4ce1bbee55f178abc
bala-amypo <bala@amypo.in>
2025-12-19 08:07:13 +0000

    User commit

--- src/main/java/com/example/demo/service/TimeStampService.java
+++ src/main/java/com/example/demo/service/TimeStampService.java
@@ -2,6 +2,5 @@ package com.example.demo.service;
 import com.example.demo.entity.TimeStamp;
 import java.util.*;
 public interface TimeStampService{
-    TimeStamp post(TimeStamp stu);
-    TimeStamp finddata1(Long id);
+    TimeStamp post1(TimeStamp stu);
 }
--- src/main/java/com/example/demo/service/impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TimeStampServiceImpl.java
@@ -9,13 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service
 public class TimeStampServiceImpl implements TimeStampService{
      @Autowired TimeStampRepo den;
-     
           @Override
-          public TimeStamp post1(TimeStamp stu){
-               return den.save(stu);
-          }
-          @Override
-          public TimeStamp finddata1(Long id){
-               return den.findById(id).orElse(null);
+          public TimeStamp post1(TimeStamp c){
+               return den.save(c);
           }
 }
