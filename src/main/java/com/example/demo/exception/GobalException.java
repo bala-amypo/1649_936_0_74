9d8d691857616ed11f01f57525e88aaea389cdaa
bala-amypo <bala@amypo.in>
2025-12-19 05:31:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/GobalException.java
+++ src/main/java/com/example/demo/exception/GobalException.java
@@ -1,6 +1,9 @@
 package com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 @RestControllerAdvice
 public class GobalException{
+@ExceptionHandler(ValidationException.class)
+public 
 
 }
