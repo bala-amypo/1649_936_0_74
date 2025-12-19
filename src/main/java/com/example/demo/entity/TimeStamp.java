8b58c9dfcb3cd6c2b3bbde45f323f91b9daf0add
bala-amypo <bala@amypo.in>
2025-12-19 07:49:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStamp.java
+++ src/main/java/com/example/demo/entity/TimeStamp.java
@@ -1,5 +1,10 @@
 package  com.example.demo.entity;
+import java.time.LocalDateTime;
 public class TimeStamp{
     private Long id;
-    private String name,email
+    private String name;
+    private String email;
+    private LocalDateTime createAt;
+    private Local
+
 }
